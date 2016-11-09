55e8f3e0ff79a307eb6068d6613f143cef0a6929
Jakob Odersky <jakob@odersky.com>
2016-11-08 16:50:04 -0800

    Remove experimental sjs repl

--- src/main/scala/workbench/WorkbenchBasePlugin.scala
+++ src/main/scala/workbench/WorkbenchBasePlugin.scala
@@ -14,10 +14,6 @@ object WorkbenchBasePlugin extends AutoPlugin {
 
   object autoImport {
     val localUrl = settingKey[(String, Int)]("localUrl")
-
-    val sjs = inputKey[Unit]("Run a command via the sjs REPL, which compiles it to Javascript and runs it in the browser")
-    val replFile = taskKey[File]("The temporary file which holds the source code for the currently executing sjs REPL")
-    val sjsReset = taskKey[Unit]("Reset the currently executing sjs REPL")
   }
   import autoImport._
   import ScalaJSPlugin.AutoImport._
@@ -46,78 +42,7 @@ object WorkbenchBasePlugin extends AutoPlugin {
       server.value.kill()
       state
     }}
-  ) ++ inConfig(Compile)(Seq(
-    artifactPath in sjs := crossTarget.value / "repl.js",
-    replFile := {
-      val f = sourceManaged.value / "repl.scala"
-      sbt.IO.write(f, replHistory.mkString("\n"))
-      f
-    },
-    sources in Compile += replFile.value,
-    sjs := Def.inputTaskDyn {
-      import sbt.complete.Parsers._
-      val str = sbt.complete.Parsers.any.*.parsed.mkString
-      val newSnippet = s"""
-          @scalajs.js.annotation.JSExport object O${replHistory.length}{
-            $str
-          };
-          import O${replHistory.length}._
-        """
-      replHistory.append(newSnippet)
-      Def.taskDyn {
-        // Basically C&Ped from fastOptJS, since we dont want this
-        // special mode from triggering updateBrowsers or similar
-        val s = streams.value
-        val output = (artifactPath in sjs).value
-
-        val taskCache = WritableFileVirtualTextFile(s.cacheDirectory / "fastopt-js")
-
-        sbt.IO.createDirectory(output.getParentFile)
-
-        val relSourceMapBase =
-          if ((relativeSourceMaps in fastOptJS).value)
-            Some(output.getParentFile.toURI())
-          else None
-
-        // TODO: re-enable this feature for latest scalajs
-        // NOTE: maybe use 'scalaJSOptimizerOptions in fullOptJS'
-        // (scalaJSOptimizer in fastOptJS).value.optimizeCP(
-        //   (scalaJSPreLinkClasspath in fastOptJS).value,
-        //   Config(
-        //     output = WritableFileVirtualJSFile(output),
-        //     cache = None,
-        //     wantSourceMap = (emitSourceMaps in fastOptJS).value,
-        //     relativizeSourceMapBase = relSourceMapBase,
-        //     checkIR = (scalaJSOptimizerOptions in fastOptJS).value.checkScalaJSIR,
-        //     disableOptimizer = (scalaJSOptimizerOptions in fastOptJS).value.disableOptimizer,
-        //     batchMode = (scalaJSOptimizerOptions in fastOptJS).value.batchMode
-        //     ),
-        //   s.log
-        // )
-        // end of C&P
-        val outPath = sbt.IO.relativize(
-          baseDirectory.value,
-          (artifactPath in sjs).value
-        ).get
-
-        sbt.IO.write(
-          (artifactPath in sjs).value,
-          sbt.IO.read(output) + s"\n\nO${replHistory.length - 1}()"
-        )
-        Def.task {
-          server.value.Wire[Api].run(
-            s"http://localhost:12345/$outPath"
-          ).call()
-          ()
-        }
-      }.dependsOn(packageJSDependencies, packageScalaJSLauncher, compile)
-    },
-    sjsReset := {
-      println("Clearing sjs REPL History")
-      replHistory.clear()
-    },
-    sjsReset := sjsReset.triggeredBy(fastOptJS)
-  ))
+  )
 
   override def projectSettings = workbenchSettings
 
