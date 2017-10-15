24aa196613de98b236e736bb4359a0340f5c4065
Søren Haagerup <soren@artofweb.dk>
2017-10-15 15:15:58 +0200

    Fix logging to JS console

--- src/main/scala/workbench/WorkbenchBasePlugin.scala
+++ src/main/scala/workbench/WorkbenchBasePlugin.scala
@@ -7,7 +7,16 @@ import org.scalajs.sbtplugin.ScalaJSPlugin
 import org.scalajs.core.tools.io._
 import org.scalajs.sbtplugin.ScalaJSPluginInternal._
 import org.scalajs.sbtplugin.Implicits._
-import scala.collection._
+//import scala.collection._
+
+import org.apache.logging.log4j.message._
+
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.core.{ LogEvent => Log4JLogEvent, _ }
+import org.apache.logging.log4j.core.Filter.Result
+import org.apache.logging.log4j.core.appender.AbstractAppender
+import org.apache.logging.log4j.core.layout.PatternLayout
+
 
 object WorkbenchBasePlugin extends AutoPlugin {
 
@@ -26,19 +35,33 @@ object WorkbenchBasePlugin extends AutoPlugin {
 
   val workbenchSettings = Seq(
     localUrl := ("localhost", 12345),
-    /*(extraLoggers in ThisBuild) := {
-      val clientLogger = FullLogger{
-        new Logger {
-          def log(level: Level.Value, message: => String) =
-            if(level >= Level.Info) server.value.Wire[Api].print(level.toString, message).call()
-          def success(message: => String) = server.value.Wire[Api].print("info", message).call()
-          def trace(t: => Throwable) = server.value.Wire[Api].print("error", t.toString).call()
+    (extraLoggers in ThisBuild) := {
+      val clientLogger = new AbstractAppender(
+        "FakeAppender", 
+        null,
+        PatternLayout.createDefaultLayout()) 
+      {
+        override def append(event: Log4JLogEvent): Unit = {
+
+          val level = sbt.internal.util.ConsoleAppender.toLevel(event.getLevel)
+          val message = event.getMessage
+
+          message match {
+            case o: ObjectMessage        => {
+              o.getParameter match {
+                case e : sbt.internal.util.StringEvent => server.value.Wire[Api].print(level.toString, e.message).call()
+                case e : sbt.internal.util.ObjectEvent[_] => server.value.Wire[Api].print(level.toString, e.message.toString).call()
+                case _ => server.value.Wire[Api].print(level.toString, message.getFormattedMessage()).call()
+              }
+            }
+            case _ => server.value.Wire[Api].print(level.toString, message.getFormattedMessage()).call()
+          }
         }
       }
-      clientLogger.setSuccessEnabled(true)
+      clientLogger.start()
       val currentFunction = extraLoggers.value
       (key: ScopedKey[_]) => clientLogger +: currentFunction(key)
-    },*/
+    },
     server := new Server(localUrl.value._1, localUrl.value._2),
     (onUnload in Global) := { (onUnload in Global).value.compose{ state =>
       server.value.kill()
